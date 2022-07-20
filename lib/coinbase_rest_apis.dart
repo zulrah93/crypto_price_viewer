823a29319fc2b04dae17515b525767f1e9f6da74
zulrah <1s16slrse@mozmail.com>
2022-07-20 17:24:32 -0400

    Add async calls that returns the fiats and tokens that are supported on Coinbase

--- lib/coinbase_rest_apis.dart
+++ lib/coinbase_rest_apis.dart
@@ -3,19 +3,37 @@ import 'dart:convert' as decoder;
 
 //Returns all fiat currencies this excludes coins and tokens like BTC or ADA
 Future<List<String>> get_all_fiat_currencies() async {
-      var restEndPoint = "https://api.coinbase.com/v2/currencies";
-      var response = await http.get(Uri.parse(restEndPoint));
-      var jsonResponse =
-      decoder.jsonDecode(response.body) as Map<String, dynamic>;
-      return List.filled(1, ""); // TODO: Build list of fiat currencies from jsonResponse
+  var restEndPoint = "https://api.coinbase.com/v2/currencies";
+  var response = await http.get(Uri.parse(restEndPoint));
+  var jsonResponse = decoder.jsonDecode(response.body) as Map<String, dynamic>;
+  var fiatInfos = jsonResponse['data'];
+  var amountOfFiats = fiatInfos.length;
+  var fiats = <String>[];
+  for (var i = 0; i < amountOfFiats; i++) {
+    fiats.add(fiatInfos[i]["id"]);
+  }
+  return fiats;
 }
 
 //Get's the trading pair price for example BTC to USD will return the price of BTC in USD.
-Future<String> get_price_from_trading_pair(String baseCurrency, String fiatCurrency) async {
+Future<String> get_price_from_trading_pair(
+    String baseCurrency, String fiatCurrency) async {
   var tradingPair = "$baseCurrency-$fiatCurrency";
   var restEndpoint = "https://api.coinbase.com/v2/prices/$tradingPair/sell";
   var response = await http.get(Uri.parse(restEndpoint));
-  var jsonResponse =
-  decoder.jsonDecode(response.body) as Map<String, dynamic>;
+  var jsonResponse = decoder.jsonDecode(response.body) as Map<String, dynamic>;
   return jsonResponse['data']['amount'];
+}
+
+//Returns all coins/tokens that are available to trade on the Coinbase exchange.
+Future<List<String>> get_all_coins_and_tokens() async {
+  var restEndPoint = "https://api.pro.coinbase.com/currencies";
+  var response = await http.get(Uri.parse(restEndPoint));
+  var coinInfos = decoder.jsonDecode(response.body) as List<dynamic>;
+  var amountOfFiats = coinInfos.length;
+  var coins = <String>[];
+  for (var i = 0; i < amountOfFiats; i++) {
+    coins.add(coinInfos[i]["id"]);
+  }
+  return coins;
 }
