33a565027581c74ac426f40dadaf1b126b33e5ca
zulrah <1s16slrse@mozmail.com>
2022-07-22 12:15:49 -0400

    Change HomeScreen widget to a stateful widget since we will be dynamically updating a ListView widget

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:crypto_price_viewer/constants.dart';
 import 'package:crypto_price_viewer/components/crypto_price_pair.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
